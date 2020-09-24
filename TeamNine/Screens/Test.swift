8f81f2676a0b08a1f40b5ff05467cea40fefb2af
hyunho.lee <mizzking75@gmail.com>
2020-09-25 00:48:53 +0900

    Add basic test case

---
+++ TeamNine/Screens/Test.swift
@@ -0,0 +1,23 @@
+//
+//  Test.swift
+//  TeamNine
+//
+//  Created by 이현호 on 2020/09/25.
+//  Copyright © 2020 이현호. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    var aaa: Int
+    var bbb: Int
+    
+    init(aaa: Int,bbb: Int) {
+        self.aaa = aaa
+        self.bbb = bbb
+    }
+    
+    func getSum(aaa:Int, bbb:Int) -> Int{
+        return aaa+bbb
+    }
+}
