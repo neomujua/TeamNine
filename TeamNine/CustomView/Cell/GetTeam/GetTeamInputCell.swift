0fb92e555674541e3c042d7c78e9505233e1962e
hyunho.lee <mizzking75@gmail.com>
2020-10-16 01:14:32 +0900

    Update getTeam

---
+++ TeamNine/CustomView/Cell/GetTeam/GetTeamInputCell.swift
@@ -0,0 +1,22 @@
+//
+//  GetTeamInputCell.swift
+//  TeamNine
+//
+//  Created by 이현호 on 2020/10/16.
+//  Copyright © 2020 이현호. All rights reserved.
+//
+
+import UIKit
+
+class GetTeamInputCell: UITableViewCell, TableItemPresenter {
+    var cellIdentifier: Constant.CellIdentifier = .settingsListCell
+    
+    @IBOutlet weak var placeholerLabel: UITextField!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func setDisplayItem(item: TableItemPresentable) {
+        placeholerLabel.placeholder = item.title
+    }
+}
--- TeamNine/CustomView/Cell/TeamList/TeamListCell.swift
+++ TeamNine/CustomView/Cell/TeamList/TeamListCell.swift
@@ -14,32 +14,20 @@ class TeamListCell: UITableViewCell {
     @IBOutlet weak var gameTitle: UILabel!
     @IBOutlet weak var enrollButton: UIButton!
     @IBOutlet weak var userLevel: UILabel!
-    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    //let storyboard = UIStoryboard(name: "Main", bundle: nil)
     override func awakeFromNib() {
         super.awakeFromNib()
         enrollButton.setTitleColor(.white, for: .normal)
         enrollButton.layer.cornerRadius = 6.0
     }
 
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        print("hi")
-        let pushVC = self.storyboard.instantiateViewController(identifier: "DetailTeamViewController")
-        let parentVC = self.storyboard.instantiateViewController(identifier: "TeamListViewController")
-        parentVC.navigationController?.pushViewController(pushVC, animated: true)
-        // Configure the view for the selected state
+
     }
     @IBAction func touchedEnrollButton(_ sender: Any) {
-//        let pushVC = self.storyboard.instantiateViewController(identifier: "DetailTeamViewController")
-//        let parentVC = self.storyboard.instantiateViewController(identifier: "TeamListViewController")
-//        parentVC.navigationController?.pushViewController(pushVC, animated: true)
         print("touched")
     }
-    
-    /*
-     if cell!.enrollButton.isTouchInside {
-         let pushVC = self.storyboard?.instantiateViewController(identifier: "DetailTeamViewController")
-         self.navigationController?.pushViewController(pushVC!, animated: true)
-     }
-     */
+
 }
